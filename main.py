13af9893c117a037987df1845af52baf5c90c9a6
Aptukov <84273198+Aptukov@users.noreply.github.com>
2024-03-31 15:21:11 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,43 +1,88 @@
 # Импортируем нужные библиотеки
 import pandas as pd
-from summarizer import Summarizer
-from transformers import BertTokenizer, BertModel
+from transformers import BertTokenizer, BertLMHeadModel
 from bert_score import score
+import json
+import torch
 
-# Загрузка данных
-train_data = pd.read_csv('gazeta_train.jsonl',on_bad_lines='skip')
-test_data = pd.read_csv('gazeta_test.jsonl',on_bad_lines='skip')
-val_data = pd.read_csv('gazeta_val.jsonl',on_bad_lines='skip')
+import nltk
+nltk.download('punkt')
+from nltk.tokenize import sent_tokenize
+from nltk.probability import FreqDist
+from collections import defaultdict
 
-# Объединяем значения из всех столбцов в одну строку
-train_data['text'] = train_data.apply(lambda row: ' '.join(row.values.astype(str)), axis=1)
-train_data = train_data['text']
+# Считывание данных из файлов
+with open('gazeta_train.jsonl', 'r', encoding='utf-8') as file:
+    train = file.readlines()
+train_json = [json.loads(line) for line in train]
+train_data = pd.DataFrame(train_json)
 
-value = train_data.iloc[0]
+with open('gazeta_test.jsonl', 'r', encoding='utf-8') as file:
+    test = file.readlines()
+test_json = [json.loads(line) for line in test]
+test_data = pd.DataFrame(test_json)
 
-# Инициализация экстрактивного суммаризатора
-extractive_summarizer = Summarizer()
+
+
+# Задаём параметр сжатия экстрактивным подходом (20%)
+ratio = 0.2
+
+# Функция для экстрактивной суммаризации
+def extractive_summarization(text, ratio):
+    sentences = sent_tokenize(text)
+
+    # Подсчет частоты встречаемости слов
+    words = [word for sentence in sentences for word in nltk.word_tokenize(sentence.lower()) if word.isalnum()]
+    freq = FreqDist(words)
+
+    ranking = defaultdict(int)
+
+    for i, sentence in enumerate(sentences):
+        for word in nltk.word_tokenize(sentence.lower()):
+            if word in freq:
+                ranking[i] += freq[word]
+
+    # Сортировка предложений по рейтингу
+    ranked_sentences = sorted(((rank, i) for i, rank in ranking.items()), reverse=True)
+
+    # Вычисление количества предложений для суммаризации
+    num_sentences = int(len(sentences) * ratio)
+
+    # Извлечение топ предложений
+    summary = ' '.join(sentences[idx] for _, idx in ranked_sentences[:num_sentences])
+
+    return summary
+
 
 # Функция для абстрактивной суммаризации
 def abstractive_summarization(text):
-    abstractive_summarizer = BertModel.from_pretrained('bert-base-uncased')
+    # Загрузим предварительно обученную модель BERT и токенизатор
     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-    inputs = tokenizer(text, return_tensors='pt', max_length=512, truncation=True)
-    abstractive_summary = abstractive_summarizer.generate(**inputs, max_length=150, min_length=60)
-    return abstractive_summary
+    model = BertLMHeadModel.from_pretrained('bert-base-uncased',is_decoder=True)
 
-# Применение экстрактивного суммаризатора, а затем абстрактивного суммаризатора на каждую новость
+    # Токенизируем и добавляем специальные токены [CLS] and [SEP]
+    inputs = tokenizer.encode("summarize: " + text, return_tensors="pt", max_length=150, truncation=True)
+
+    # Получаем выходы модели
+    outputs = model.generate(inputs, max_new_tokens=10, min_length=30, length_penalty=1.0, num_beams=4, early_stopping=True)
+
+    # Декодируем сгенерированный текст
+    summary = tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+    return summary
+
+# Применение экстрактивного суммаризатора, а затем абстрактивного суммаризатора на каждый текст
 predicted_summaries = []
-for text in data['text']:
-    extractive_summary = extractive_summarizer(text, min_length=60, max_length=150)
+for text in train_data['text']:
+    extractive_summary = extractive_summarization(text, ratio)
     abstractive_summary = abstractive_summarization(extractive_summary)
     predicted_summaries.append(abstractive_summary)
 
 # Добавление результатов в новый столбец 'predicted'
-data['predicted'] = predicted_summaries
+train_data['predicted'] = predicted_summaries
 
 # Создание списка справочных кратких описаний для метрики Bert score
-references = data['text'].tolist()
+references = train_data['text'].tolist()
 
 # Проверка работы суммаризатора с помощью метрики Bert score
 bert_scores = []
@@ -54,4 +99,4 @@ average_bert_score = data['bert_score'].mean()
 print(f'Средний результат метрики Bert score: {average_bert_score}')
 
 # Сохранение результата в новый файл
-data.to_csv('news_with_predicted_summaries.csv', index=False)
+train_data.to_csv('text_with_predicted', index=False)
